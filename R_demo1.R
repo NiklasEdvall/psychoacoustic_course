c8d802579570d1070ee34a986f998e66f44746b1
NiklasEdvall <niklasedvall@gmail.com>
2022-12-06 11:52:45 +0100

    updates from lecture 1

--- R_demo1.R
+++ R_demo1.R
@@ -6,4 +6,23 @@
 # summary()
 # hist()
 
-1,2,3,2,4,1,2,3,4,5,6,7,3,1,2,3,4,5,6,1,2,3,2,1
+# skapa data
+data = c(1,2,3,2,4,1,2,3,4,5,6,7,3,1,2,3,4,5,6,1,2,3,2,1)
+
+#summera data
+sum(data)
+
+#beräkna medelvärde
+mean(data)
+
+#beräkna sd
+sd(data)
+
+#antal i data
+length(data)
+
+#sammanfatta data
+summary(data)
+
+#histogram
+hist(data)
